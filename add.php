07bd0f7c2f29acf4a99f6ec5aff34b7d5cafde02
Igor Kashirskiy <igor.kashirsky@gmail.com>
2020-07-20 16:48:47 +0500

    Refactoring is made, ready for MVC. The 4th commit.

--- add.php
+++ add.php
@@ -3,10 +3,14 @@
 require_once 'db.php';
 
 if (!empty($_POST)) {
+    $id = $_POST['id'] ?? '';
     $name = $_POST['name'] ?? '';
-    $year = $_POST['year'] ?? '';
+    $article = $_POST['article'] ?? '';
+    $price = $_POST['price'] ?? '';
+    $amount = $_POST['amount'] ?? '';
+    $description = $_POST['description'] ?? '';
 
-    $query = "INSERT INTO products(name) VALUES ('$name')";
+    $query = "INSERT INTO products(name, article, price, amount, description) VALUES ('$name', '$article', '$price', '$amount', '$description')";
     $result = query($connect, $query);
 
     if (mysqli_affected_rows($connect)) {
@@ -22,7 +26,30 @@ if (!empty($_POST)) {
 <br>
 <form method="post" action="">
     <label>
-        Название книги: <input type="text" name="name" required>
+        Название товара: <input type="text" name="name" required">
     </label>
+    <br>
+    <br>
+    <label>
+        Артикул: <input type="text" name="article" required>
+    </label>
+    <br>
+    <br>
+    <label>
+        Цена: <input type="number" name="price" required>
+    </label>
+    <br>
+    <br>
+    <label>
+        Количество на складе: <input type="numb" name="amount">
+    </label>
+    <br>
+    <br>
+    <label>
+        Описание товара: <br>
+        <textarea name="description" cols="30" rows="10" required></textarea>
+    </label>
+    <br>
+    <br>
     <input type="submit" value="Добавить">
 </form>
--- edit.php
+++ edit.php
@@ -19,8 +19,12 @@ if ($id) {
 if (!empty($_POST)) {
     $id = $_POST['id'] ?? '';
     $name = $_POST['name'] ?? '';
+    $article = $_POST['article'] ?? '';
+    $price = $_POST['price'] ?? '';
+    $amount = $_POST['amount'] ?? '';
+    $description = $_POST['description'] ?? '';
 
-    $query = "UPDATE products SET name = '$name' WHERE id = $id";
+    $query = "UPDATE products SET name = '$name', article = '$article', price = '$price', amount = '$amount', description = '$description' WHERE id = $id";
     $result = query($connect, $query);
 
     if (mysqli_affected_rows($connect)) {
@@ -36,7 +40,30 @@ if (!empty($_POST)) {
 <form method="post" action="">
     <input type="hidden" name="id" value="<?php echo $product['id'] ?? 0; ?>">
     <label>
-        Название книги: <input type="text" name="name" required value="<?php echo $product['name'] ?? ''; ?>">
+        Название товара: <input type="text" name="name" required value="<?php echo $product['name'] ?? ''; ?>">
     </label>
+    <br>
+    <br>
+    <label>
+        Артикул: <input type="text" name="article" required value="<?php echo $product['article'] ?? ''; ?>">
+    </label>
+    <br>
+    <br>
+    <label>
+        Цена: <input type="number" name="price" required value="<?php echo $product['price'] ?? ''; ?>">
+    </label>
+    <br>
+    <br>
+    <label>
+        Количество на складе: <input type="number" name="amount" required value="<?php echo $product['amount'] ?? ''; ?>">
+    </label>
+    <br>
+    <br>
+    <label>
+        Описание товара: <br>
+        <textarea name="description" cols="30" rows="10"><?php echo $product['description'] ?? ''; ?></textarea>
+    </label>
+    <br>
+    <br>
     <input type="submit" value="Сохранить">
 </form>
